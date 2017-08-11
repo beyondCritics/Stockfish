b6e5c5b50761ac9316982ec7f1043c7c1c09cb18
[Oliver Roese] <oroese@yahoo.de>
2017-08-11 16:47:53 +0200

    Respect skip_quiets flag
    Skip sorting of bad quiets, if "skip_quiets" is true during scoring of quiets. It is assumed within next_move(), that skip_quiets is sticky, which is true globally currently and sensible in any case.

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -213,7 +213,8 @@ Move MovePicker::next_move(bool skipQuiets) {
       cur = endBadCaptures;
       endMoves = generate<QUIETS>(pos, cur);
       score<QUIETS>();
-      partial_insertion_sort(cur, endMoves, -4000 * depth / ONE_PLY);
+      partial_insertion_sort(cur, endMoves,
+              (skipQuiets) ? VALUE_ZERO : -4000 * depth / ONE_PLY);
       ++stage;
       /* fallthrough */
 
